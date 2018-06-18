03869307b9608c82d79c135831339dbdb3524a88
mashuxa <mashuxa@tut.by>
2018-06-18 16:22:10 +0300

    added html

---
+++ modules/app/js/app.js
@@ -0,0 +1,10 @@
+ // (function () {
+ //     "use strict";
+
+     // var docx2html=require('docx2html');
+     // docx2html("../../../content/docx/1Z0-061.docx").then(function(html){
+     //     console.log(html);
+     // });
+
+
+ // }());
